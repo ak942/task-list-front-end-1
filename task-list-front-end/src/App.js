0169d7ad8b89bbcc5f73fca50222d2df4503db48
Agnes Kim <agneskim4@gmail.com>
2023-06-21 11:34:12 -0400

    update app CSS

--- task-list-front-end/src/App.js
+++ task-list-front-end/src/App.js
@@ -1,17 +1,50 @@
-import './App.css';
 import React from 'react';
+import TaskList from './components/TaskList.js';
+import './App.css';
 
-function App() {
+const TASKS = [
+  {
+    id: 1,
+    title: 'Mow the lawn',
+    isComplete: false,
+  },
+  {
+    id: 2,
+    title: 'Cook Pasta',
+    isComplete: true,
+  },
+];
+
+const App = () => {
+  const [tasks, setTask] = React.useState(TASKS);
+
+  const toggleTask = (id) => {
+    setTask(prevTask => {
+      const updatedtask = prevTask.map(task => {
+        return task.id === id ? {...task, isComplete: !task.isComplete} : task
+      })
+    return updatedtask;
+    })
+  };
+
+  const removeTask = (id) => {
+    setTask(prevTask => {
+      const updatedtask = prevTask.filter(task => task.id !==id);
+      return updatedtask;
+    })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1> Task List </h1>
+        <h1>Ada&apos;s Task List</h1>
       </header>
       <main>
-
+        <div>{<TaskList tasks={tasks} toggleTask = {toggleTask} removeTask = {removeTask}/>}</div>
       </main>
     </div>
   );
-}
+};
 
 export default App;
+
