c9c8c414465f85d4156164f9ed715475a78fe97f
Agnes Kim <agneskim4@gmail.com>
2023-06-21 13:18:10 -0400

    add useEffect to call backend API

--- task-list-front-end/src/App.js
+++ task-list-front-end/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import TaskList from './components/TaskList.js';
 import './App.css';
-const test=1
+import axios from 'axios'
+
 const TASKS = [
   {
     id: 1,
@@ -17,6 +18,19 @@ const TASKS = [
 
 const App = () => {
   const [tasks, setTask] = React.useState(TASKS);
+
+  React.useEffect(()=> {
+    console.log("useEffect")
+    const getAllData = async () => {
+      try {
+      const response = await axios.get('https://agnes-task-list-api.onrender.com/tasks')
+      console.log(response)
+    } catch (err) {
+      console.log(err.message)
+    }
+  }
+    return getAllData
+  },[])
 
   const toggleTask = (id) => {
     setTask(prevTask => {
@@ -37,7 +51,7 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Ada&apos;s Task List</h1>
+        <h1>Task List</h1>
       </header>
       <main>
         <div>{<TaskList tasks={tasks} toggleTask = {toggleTask} removeTask = {removeTask}/>}</div>
